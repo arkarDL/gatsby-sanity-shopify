57300ccde72149d52f1f6cc3180e7f8650774b4d
HtetMyatLinn <htet.myat.linn@digitallaboratory.net>
2022-09-12 09:27:52 +0630

    i18n Sanity

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,65 +1,65 @@
 // First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
+import createSchema from "part:@sanity/base/schema-creator"
 // Then import schema types from any plugins that might expose them
-import schemaTypes from "all:part:@sanity/base/schema-type";
+import schemaTypes from "all:part:@sanity/base/schema-type"
 
 // Rich text annotations used in the block content editor
-import annotationLinkEmail from "./annotations/linkEmail";
-import annotationLinkExternal from "./annotations/linkExternal";
-import annotationLinkInternal from "./annotations/linkInternal";
-import annotationProduct from "./annotations/product";
+import annotationLinkEmail from "./annotations/linkEmail"
+import annotationLinkExternal from "./annotations/linkExternal"
+import annotationLinkInternal from "./annotations/linkInternal"
+import annotationProduct from "./annotations/product"
 
 // Document types
-import collection from "./documents/collection";
-import colorTheme from "./documents/colorTheme";
-import page from "./documents/page";
-import product from "./documents/product";
-import productVariant from "./documents/productVariant";
+import collection from "./documents/collection"
+import colorTheme from "./documents/colorTheme"
+import page from "./documents/page"
+import product from "./documents/product"
+import productVariant from "./documents/productVariant"
 
 // Singleton document types
-import home from "./singletons/home";
-import settings from "./singletons/settings";
+import home from "./singletons/home"
+import settings from "./singletons/settings"
 
 // Block content
-import body from "./blocks/body";
+import body from "./blocks/body"
 
 // Object types
-import collectionRule from "./objects/collectionRule";
-import customProductOptionColor from "./objects/customProductOption/color";
-import customProductOptionSize from "./objects/customProductOption/size";
-import imageWithProductHotspots from "./objects/imageWithProductHotspots";
-import linkExternal from "./objects/linkExternal";
-import linkInternal from "./objects/linkInternal";
-import heroCollection from "./objects/hero/collection";
-import heroHome from "./objects/hero/home";
-import heroPage from "./objects/hero/page";
-import moduleAccordion from "./objects/module/accordion";
-import moduleCallout from "./objects/module/callout";
-import moduleCallToAction from "./objects/module/callToAction";
-import moduleCollection from "./objects/module/collection";
-import moduleGrid from "./objects/module/grid";
-import moduleImage from "./objects/module/image";
-import moduleImages from "./objects/module/images";
-import moduleInstagram from "./objects/module/instagram";
-import moduleProduct from "./objects/module/product";
-import moduleProducts from "./objects/module/products";
-import placeholderString from "./objects/placeholderString";
-import productHotspots from "./objects/productHotspots";
-import productOption from "./objects/productOption";
-import productWithVariant from "./objects/productWithVariant";
-import proxyString from "./objects/proxyString";
-import seoHome from "./objects/seo/home";
-import seoPage from "./objects/seo/page";
-import seoShopify from "./objects/seo/shopify";
-import shopifyCollection from "./objects/shopifyCollection";
-import shopifyProduct from "./objects/shopifyProduct";
-import shopifyProductVariant from "./objects/shopifyProductVariant";
+import collectionRule from "./objects/collectionRule"
+import customProductOptionColor from "./objects/customProductOption/color"
+import customProductOptionSize from "./objects/customProductOption/size"
+import imageWithProductHotspots from "./objects/imageWithProductHotspots"
+import linkExternal from "./objects/linkExternal"
+import linkInternal from "./objects/linkInternal"
+import heroCollection from "./objects/hero/collection"
+import heroHome from "./objects/hero/home"
+import heroPage from "./objects/hero/page"
+import moduleAccordion from "./objects/module/accordion"
+import moduleCallout from "./objects/module/callout"
+import moduleCallToAction from "./objects/module/callToAction"
+import moduleCollection from "./objects/module/collection"
+import moduleGrid from "./objects/module/grid"
+import moduleImage from "./objects/module/image"
+import moduleImages from "./objects/module/images"
+import moduleInstagram from "./objects/module/instagram"
+import moduleProduct from "./objects/module/product"
+import moduleProducts from "./objects/module/products"
+import placeholderString from "./objects/placeholderString"
+import productHotspots from "./objects/productHotspots"
+import productOption from "./objects/productOption"
+import productWithVariant from "./objects/productWithVariant"
+import proxyString from "./objects/proxyString"
+import seoHome from "./objects/seo/home"
+import seoPage from "./objects/seo/page"
+import seoShopify from "./objects/seo/shopify"
+import shopifyCollection from "./objects/shopifyCollection"
+import shopifyProduct from "./objects/shopifyProduct"
+import shopifyProductVariant from "./objects/shopifyProductVariant"
 
 // Custom contents
-import blockContent from "./blogs/blockContent";
-import category from "./blogs/category";
-import post from "./blogs/post";
-import author from "./blogs/author";
+import blockContent from "./blogs/blockContent"
+import category from "./blogs/category"
+import post, { localeString } from "./blogs/post"
+import author from "./blogs/author"
 
 // Build the schemas and export to the Sanity Studio app
 export default createSchema({
@@ -73,7 +73,7 @@ export default createSchema({
     category,
     post,
     author,
-
+    localeString,
     // Annotations
     annotationLinkEmail,
     annotationLinkExternal,
@@ -122,4 +122,4 @@ export default createSchema({
     shopifyProduct,
     shopifyProductVariant,
   ]),
-});
+})
