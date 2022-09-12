b1976c211cdd874c3dd8270d5740c0d033879469
HtetMyatLinn <htet.myat.linn@digitallaboratory.net>
2022-09-12 14:35:55 +0630

    Blog Details Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,41 +1,13 @@
+const path = require(`path`)
+
 exports.createPages = async function ({ actions, graphql }) {
   const { createPage } = actions
   const { data } = await graphql(`
     query AllBlogs {
       allSanityPost {
         nodes {
-          title {
-            en
-            ja
-          }
           slug {
             current
-          }
-          categories {
-            title {
-              en
-              ja
-            }
-          }
-          description {
-            en
-            ja
-          }
-          body {
-            en {
-              _key
-              _type
-              style
-              list
-              _rawChildren
-            }
-            ja {
-              _key
-              _type
-              style
-              list
-              _rawChildren
-            }
           }
         }
       }
@@ -50,7 +22,6 @@ exports.createPages = async function ({ actions, graphql }) {
     createPage({
       path,
       component: blogPostTemplate,
-
       context: {
         pagePath: path,
       },
--- src/templates/detail/index.jsx
+++ src/templates/detail/index.jsx
@@ -1,27 +1,57 @@
-import ReactMarkdown from "react-markdown"
 import React from "react"
 import Container from "../../layout/container"
-const Detail = () => {
-  const markdown = `Just a link: https://reactjs.com.`
+import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import { PortableText } from "@portabletext/react"
+
+const Detail = ({ pageContext, data }) => {
+  const article = data?.allSanityPost?.nodes[0]
+  const language = pageContext?.i18n.language
 
   return (
     <Container>
       <div className="my-8 ">
-        <h1 className="text-4xl font-bold my-4">Lorem is cool</h1>
-        <p className="text-sm ">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          rerum consequatur aperiam perferendis dignissimos eum quibusdam
-          debitis, sed culpa adipisci!
-        </p>
-        <button className="hover:border-l-2 text-sm border-blue-500 max-w-max px-4 mt-4  uppercase py-1 shadow-sm rounded-full text-blue-500">
-          Share
-        </button>
-      </div>
-
-      <div className="prose">
-        <ReactMarkdown children={markdown} />
+        <h1
+          className="font-bold my-4"
+          style={{ fontSize: 28, textTransform: "capitalize" }}
+        >
+          {article.title[language]}
+        </h1>
+        <GatsbyImage
+          image={article.mainImage.asset.gatsbyImageData}
+          className="my-6"
+        />
+        <div className="prose">
+          <PortableText value={article._rawBody[language]} />
+        </div>
       </div>
     </Container>
   )
 }
 export default Detail
+
+export const query = graphql`
+  query FetchMetaData($pagePath: String!) {
+    allSanityPost(filter: { slug: { current: { eq: $pagePath } } }) {
+      nodes {
+        slug {
+          current
+        }
+        _rawBody
+        title {
+          en
+          ja
+        }
+        mainImage {
+          asset {
+            gatsbyImageData(
+              fit: FILLMAX
+              placeholder: DOMINANT_COLOR
+              width: 400
+            )
+          }
+        }
+      }
+    }
+  }
+`
